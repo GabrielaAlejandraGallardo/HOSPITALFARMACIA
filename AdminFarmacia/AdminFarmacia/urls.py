6c9522d5b9296f03da9dae33c7ed4e7b31246830
Gabriela Gallardo <gabicde@gmail.com>
2026-05-20 11:48:35 -0300

    colapse

--- AdminFarmacia/AdminFarmacia/urls.py
+++ AdminFarmacia/AdminFarmacia/urls.py
@@ -20,6 +20,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('agente.urls')),
+    path('',include('agente.urls')),
     path('',include('deposito.urls')),
 ]
--- AdminFarmacia/deposito copy/admin.py
+++ AdminFarmacia/deposito copy/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Deposito
+from .models import Deposito,Hueco,Supervisor
 
 admin.site.register(Deposito)
+admin.site.register(Hueco)
+admin.site.register(Supervisor)
