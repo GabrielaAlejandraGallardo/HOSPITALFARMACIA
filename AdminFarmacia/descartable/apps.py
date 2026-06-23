2c7dd3c1b7a8cb3168fe8dfa4e7efdb7a0082ebf
gabi <gabicde@hotmail.com>
2026-06-23 13:47:25 -0300

    descartables

--- AdminFarmacia/descartable/apps.py
+++ AdminFarmacia/descartable/apps.py
@@ -2,13 +2,6 @@ from django.apps import AppConfig
 
 
 
-<<<<<<< HEAD
-class DescartableConfig(AppConfig):
-    name='descartable'    
-    
-class MedicamentoConfig(AppConfig):
-    name="medicamento"
-=======
 class MedicamentoConfig(AppConfig):
     name='medicamento'    
     
@@ -17,6 +10,3 @@ class DepositoConfig(AppConfig):
     
 class DescartableConfig(AppConfig):   
     name='descartable'  
->>>>>>> 3506bcb9958dee91c886cb7c09d97d3b0e703b89
-    
-    
